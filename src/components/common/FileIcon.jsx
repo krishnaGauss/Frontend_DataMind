fd0e0a94ab94549d0a0773541712705c1d51be89
krishnaGauss <goswamikrishna549@gmail.com>
2025-09-13 23:29:42 +0530

    initial directory

---
+++ src/components/common/FileIcon.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const FileIcon = () => {
+  return (
+    <div>FileIcon</div>
+  )
+}
+
+export default FileIcon
---
+++ src/components/sessions/SessionList.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SessionList = () => {
+  return (
+    <div>SessionList</div>
+  )
+}
+
+export default SessionList
